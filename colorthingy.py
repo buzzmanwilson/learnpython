2c969d0faced256c8e3c789149b04ba4262e4b97
clayton <claytonscott.w@gmail.com>
2019-02-09 14:52:37 -0700

    message here

---
+++ colorthingy.py
@@ -0,0 +1,9 @@
+def myfunction():
+	print("What color is your car?")
+	a = input()
+	if a == "blue" :
+		print("I like the color " + a + ", but my favorite color is red")
+	else :
+		print("I like " + a + ", but my favorite is blue")
+
+myfunction()
